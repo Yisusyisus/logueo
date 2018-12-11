db241f4a4a00b1e1bd8d66cf6701da5dad0ff8ad
edgar mendoza <mendozaedgar02@gmail.com>
2018-12-11 02:30:31 -0600

    instancia prueba

--- conexion.php
+++ conexion.php
@@ -1,13 +1,13 @@
 <?php
 
-$conexion = mysql_connect("localhost", "root", "")
+$conexion = mysql_connect("localhost", "ittlaxia_logueo", "qwerty")
         or die("Error de conexion" . mysql_error());
 
 if (!$conexion) {
     header("location:verificacion.php?error=Error en la Conexion de la DB");
     exit();
 }
-$db = mysql_select_db("logueo")
+$db = mysql_select_db("ittlaxia_logueo")
         or die("Error al seleccionar la base de datos" . mysql_error());
 if (!$db) {
     header("location:verificacion.php?error=Error al Seleecionar la DB");
