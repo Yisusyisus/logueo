8a6f69ff614b388d829dbdc2581cc97a4736d62b
donsfras <aldo_dons@outlook.com>
2018-12-11 23:29:57 -0600

    Update conexion.php

--- loginOO/class/conexion.php
+++ loginOO/class/conexion.php
@@ -3,6 +3,7 @@
 class Conectar // se crea una clase para el metodo de conexion 
 {
     public static function con() { // se crea una funcion estatica para la conexion
+        $conexion = mysql_connect("localhost", "ittlaxia_logueo", "qwerty");
         mysql_query("SET NAMES 'utf8'"); // para no tener problmas con la ñ
         mysql_select_db("login"); // indicamos el nombe de nuestra base de datos
         return $conexion;
