9a432d9ea475d2d62fd82b411bc85064ff00ec01
donsfras <aldo_dons@outlook.com>
2018-12-13 03:58:59 -0600

    dons

--- conexion.php
+++ conexion.php
@@ -5,7 +5,7 @@
 
 		public static function conectar(){
 			$pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
-			self::$conexion=new PDO('mysql:host=localhost;dbname=baselogin','root','',$pdo_options);
+			self::$conexion=new PDO('mysql:host=localhost;dbname=ganzo','root','',$pdo_options);
 			return self::$conexion;
 		}
 	}
